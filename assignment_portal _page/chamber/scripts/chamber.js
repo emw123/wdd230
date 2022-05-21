738fa5178aaf66d1a2fdfb9a9865f42eaec215a6
Emma <87395798+emw123@users.noreply.github.com>
2022-05-21 12:03:55 -0600

    updated chamber home page

--- assignment_portal _page/chamber/scripts/chamber.js
+++ assignment_portal _page/chamber/scripts/chamber.js
@@ -6,6 +6,11 @@ let currentdate = document.lastModified;
 document.querySelector('#lastDate').textContent = currentdate;
 console.log(document.lastModified);
 
+let day = date.getDay();
+const bannerMessage = document.querySelector('#banner');
+if (day == 1) or (day == 2);
+    bannerMessage.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
+
 const now = new Date();
 const datefield = document.querySelector("#headerdate");
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
@@ -17,3 +22,4 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerButton');
 x.onclick = toggleMenu;
+
