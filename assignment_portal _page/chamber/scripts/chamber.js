f043e17a9e9b9f415cf67a5cd3fe837b111e2e1f
Emma <87395798+emw123@users.noreply.github.com>
2022-07-02 10:44:51 -0600

    Chamber Site

--- assignment_portal _page/chamber/scripts/chamber.js
+++ assignment_portal _page/chamber/scripts/chamber.js
@@ -7,10 +7,10 @@ let currentdate = document.lastModified;
 document.querySelector('#lastDate').textContent = currentdate;
 console.log(document.lastModified);
 
-let day = date.getDay();
-const bannerMessage = document.querySelector('#banner');
-if (day == 1) or (day == 2);
-    bannerMessage.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
+//let day = date.getDay();
+//const bannerMessage = document.querySelector('#banner');
+//if (day == 1) || (day == 2);
+    //bannerMessage.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
 
 const now = new Date();
 const datefield = document.querySelector("#headerdate");
